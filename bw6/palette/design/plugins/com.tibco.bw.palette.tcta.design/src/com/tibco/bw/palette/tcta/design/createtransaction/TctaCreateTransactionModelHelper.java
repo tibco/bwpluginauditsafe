163af19effe25d3ca5de812bdf4056c44f6f2f7d
nwang <nwang@0f956438-001a-499c-b957-7c8e11d45900>
2018-07-02 22:59:24 +0000

    add input properties for createTransaction
    git-svn-id: https://svn.tibco.com/svn/amxext/trunk/bwsolutions/bwplugintcta@110350 0f956438-001a-499c-b957-7c8e11d45900

--- bw6/palette/design/plugins/com.tibco.bw.palette.tcta.design/src/com/tibco/bw/palette/tcta/design/createtransaction/TctaCreateTransactionModelHelper.java
+++ bw6/palette/design/plugins/com.tibco.bw.palette.tcta.design/src/com/tibco/bw/palette/tcta/design/createtransaction/TctaCreateTransactionModelHelper.java
@@ -3,6 +3,9 @@ package com.tibco.bw.palette.tcta.design.createtransaction;
 import org.eclipse.emf.ecore.EObject;
 
 import com.tibco.bw.design.api.BWAbstractModelHelper;
+import com.tibco.bw.design.util.ModelHelper;
+import com.tibco.bw.design.util.ProcessProperty;
+import com.tibco.bw.palette.tcta.design.TctaConstants;
 import com.tibco.bw.palette.tcta.model.tcta.TctaFactory;
 import com.tibco.bw.palette.tcta.model.tcta.TctaCreateTransaction;
 
@@ -21,4 +24,25 @@ public class TctaCreateTransactionModelHelper extends BWAbstractModelHelper {
         // end-custom-code
         return activity;
     }
+
+    public void postCreate(EObject model, Object container) {
+		if (model == null)     {  return;  }
+		if (container == null) {  return; }
+		boolean found = false;
+		for (ProcessProperty property : ModelHelper.INSTANCE.getProperties(container)) {
+			if (ModelHelper.INSTANCE.isEqual(property.getType(), TctaConstants.SHAREDRESOURCE_QNAME)) {
+				((TctaCreateTransaction) model).setTctaConnection(property.getName());
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			ProcessProperty property = ModelHelper.INSTANCE.createProperty(container, "tctaConnectionProperty", TctaConstants.SHAREDRESOURCE_QNAME, "");
+			if (property != null) {
+				((TctaCreateTransaction) model).setTctaConnection(property.getName());
+			}
+		}
+
+	}
 }
